29bbc84ce555858e17d8be6b79e9c94fd0ab7ab8
domlg <dominique.legal@gmail.com>
2026-06-01 15:50:08 +0200

    refactor: delete unused references to getNextId.

--- backend/suivi_pasto/tests/test_base_viewset.py
+++ backend/suivi_pasto/tests/test_base_viewset.py
@@ -13,10 +13,3 @@ class BaseModelViewSetTests(TestCase):
         viewset = UnitePastoraleViewset()
         viewset.queryset = UnitePastorale.objects.all()
         self.assertEqual(viewset.get_pk_field_name(), "id_unite_pastorale")
-
-    def test_get_next_id_empty_queryset(self):
-        viewset = UnitePastoraleViewset()
-        viewset.queryset = UnitePastorale.objects.none()
-        request = self.factory.get("/api/unitePastorale/getNextId")
-        response = viewset.get_next_id(request)
-        self.assertEqual(response.data.get("next_id"), 1)

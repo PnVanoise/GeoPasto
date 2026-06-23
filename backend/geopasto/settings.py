ed53e527cbd21a82322ec802a90611fd29cd1f7d
domlg <dominique.legal@gmail.com>
2026-06-23 12:19:06 +0200

    feat: permissions classes on endpoints

--- backend/geopasto/settings.py
+++ backend/geopasto/settings.py
@@ -237,7 +237,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "suivi_pasto.permissions.StrictDjangoModelPermissions",
+    ),
     "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
     "EXCEPTION_HANDLER": "suivi_pasto.exceptions.api_exception_handler",
 }
